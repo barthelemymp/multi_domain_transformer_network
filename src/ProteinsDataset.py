d0537313b5fce9d493d90738232fa8df52bcf58b
barthelemymp <barthelemy.meynard@polytechnique.edu>
2022-06-29 23:26:04 +0200

    push

--- src/ProteinsDataset.py
+++ src/ProteinsDataset.py
@@ -309,8 +309,7 @@ class ProteinNetworkDataset(torch.utils.data.Dataset):
                 else :
                     self.idx_in_clique[i,j] = -1
         self.DomainProfile = self.idx_in_clique != -1
-        self.DomainProfile = self.DomainProfile.numpy()
-        self.DomainProfile = np.array(self.DomainProfile.cpu().numpy())
+        # self.DomainProfile = np.array(self.DomainProfile.cpu().numpy())
         # self.DomainProfileMatrix = (self.DomainProfile[:, None, :] != self.DomainProfile).sum(2)
         # Z = hierarchy.ward(self.DomainProfileMatrix)
         # self.proteinOrdering = hierarchy.leaves_list(hierarchy.optimal_leaf_ordering(Z, self.DomainProfileMatrix))
